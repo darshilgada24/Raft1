c87be5879475e95dc1bfc2e27329ab231c926b53
Darshil Gada <darshilgada@Darshils-MacBook-Pro.local>
2019-11-30 14:03:21 -0500

    build=lab3

--- src/kvraft/client.go
+++ src/kvraft/client.go
@@ -1,13 +1,18 @@
 package raftkv
 
-import "labrpc"
+import (
+	"labrpc"
+	"time"
+)
 import "crypto/rand"
 import "math/big"
-
 
 type Clerk struct {
 	servers []*labrpc.ClientEnd
 	// You will have to modify this struct.
+	LastLeader int // To store who was the last leader for this clerk
+	ClientId int64 // To identify each clerk
+	RequestNo int // To keep track number of get and put in the log
 }
 
 func nrand() int64 {
@@ -21,10 +26,12 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 	ck := new(Clerk)
 	ck.servers = servers
 	// You'll have to add code here.
+	ck.ClientId = nrand()
+	ck.RequestNo = 0
+	ck.LastLeader = 0
 	return ck
 }
 
-//
 // fetch the current value for a key.
 // returns "" if the key does not exist.
 // keeps trying forever in the face of all other errors.
@@ -39,10 +46,34 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 func (ck *Clerk) Get(key string) string {
 
 	// You will have to modify this function.
-	return ""
+	ck.RequestNo++
+	//fmt.Printf("Inside Get for clerk %d",ck.ClientId)
+	//fmt.Println()
+	i:= ck.LastLeader
+	//fmt.Printf(" Get Request: Key %s, client Id %d",key,ck.ClientId)
+	//fmt.Println()
+	for {
+		args := GetArgs{key, ck.ClientId, ck.RequestNo}
+		reply := GetReply{}
+		ok := ck.servers[i].Call("KVServer.Get", &args, &reply)
+
+		if ok && reply.WrongLeader == false && reply.Err == OK{
+			//fmt.Printf("Value received for get %s" ,reply.Value)
+			//fmt.Println()
+			ck.LastLeader = i
+			return reply.Value
+		} else if ok && reply.WrongLeader == false && reply.Err == ErrNoKey{
+			//fmt.Println("key not present for get ")
+			ck.LastLeader = i
+			return ""
+		} else {
+			i = (i + 1) % len(ck.servers)
+		}
+		time.Sleep(100 * time.Millisecond)
+	}
 }
 
-//
+
 // shared by Put and Append.
 //
 // you can send an RPC with code like this:
@@ -51,14 +82,35 @@ func (ck *Clerk) Get(key string) string {
 // the types of args and reply (including whether they are pointers)
 // must match the declared types of the RPC handler function's
 // arguments. and reply must be passed as a pointer.
-//
+
 func (ck *Clerk) PutAppend(key string, value string, op string) {
 	// You will have to modify this function.
+	//fmt.Printf("Inside Put Append for clerk %d",ck.ClientId)
+	//fmt.Println()
+	ck.RequestNo++
+	i:= ck.LastLeader
+	//fmt.Printf(" PutAppend Request: Key %s, Value %s, client Id %d",key,value,ck.ClientId)
+	//fmt.Println()
+	for {
+		args := PutAppendArgs{key, value,op,ck.ClientId,ck.RequestNo}
+		reply := PutAppendReply{}
+		ok := ck.servers[i].Call("KVServer.PutAppend", &args, &reply)
+
+		if ok && reply.WrongLeader == false && reply.Err == OK{
+			ck.LastLeader = i
+			return
+		} else {
+			i = (i + 1) % len(ck.servers)
+		}
+		time.Sleep(100 * time.Millisecond)
+	}
 }
 
 func (ck *Clerk) Put(key string, value string) {
+	//fmt.Println("Put Called")
 	ck.PutAppend(key, value, "Put")
 }
 func (ck *Clerk) Append(key string, value string) {
+	//fmt.Println("Append Called")
 	ck.PutAppend(key, value, "Append")
 }
--- src/kvraft/server.go
+++ src/kvraft/server.go
@@ -1,11 +1,15 @@
 package raftkv
 
 import (
+	"fmt"
+	//"fmt"
+	//"fmt"
 	"labgob"
 	"labrpc"
 	"log"
 	"raft"
 	"sync"
+	"time"
 )
 
 const Debug = 0
@@ -17,13 +21,6 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 	return
 }
 
-
-type Op struct {
-	// Your definitions here.
-	// Field names must start with capital letters,
-	// otherwise RPC will break.
-}
-
 type KVServer struct {
 	mu      sync.Mutex
 	me      int
@@ -31,17 +28,104 @@ type KVServer struct {
 	applyCh chan raft.ApplyMsg
 
 	maxraftstate int // snapshot if log grows this big
+	persister    *raft.Persister
 
 	// Your definitions here.
-}
+	DataStore map[string] string //Key Value Data store
+	ClientToResponseMapping map[int64] chan Response //Client to response Mapping
+	duplicates map[int64] int // Client to RequestNo Map
 
+}
+type Op struct {
+	// Your definitions here.
+	// Field names must start with capital letters,
+	// otherwise RPC will break.
+	Key string
+	Value string
+	TypeOfCommand string
+	RequestNo int // To check for stale data if index from ApplyCh is not same
+	ClientId int64
+}
 
 func (kv *KVServer) Get(args *GetArgs, reply *GetReply) {
 	// Your code here.
+	//op:= Op{Key:args.Key,TypeOfCommand:"Get",ClientId:args.ClientId,RequestNo:args.RequestNo}
+	fmt.Println("Inside Get Procedure call")
+	_, _, isLeader := kv.rf.Start(Op{args.Key, "", "Get", args.RequestNo, args.ClientId})
+
+	if isLeader == false {
+		fmt.Println("Wrong Leader for Get")
+		reply.WrongLeader = true
+	} else {
+			kv.mu.Lock()
+		    _,clientIdPresent := kv.ClientToResponseMapping[args.ClientId]
+
+		    if clientIdPresent == false {
+				fmt.Println("Client ID not present for get")
+		    	ch:= make(chan Response,1)
+		    	kv.ClientToResponseMapping[args.ClientId] = ch
+			}
+		    kv.mu.Unlock()
+			select {
+			case  response :=<- kv.ClientToResponseMapping[args.ClientId]:
+				 	kv.mu.Lock()
+
+				 	if response.RequestNo == args.RequestNo && response.ClientId == args.ClientId && args.Key == response.Key{ //Only consider requests which have same client ID and request No
+				 	// otherwise incorrect result is returned
+				 		value, keyPresent := kv.DataStore[response.Key]
+					 	if keyPresent == false {
+							fmt.Println("Key not present")
+						 	reply.WrongLeader = false
+						 	reply.Err = ErrNoKey
+					 	} else {
+						 	fmt.Println("Key Present for Get")
+						 	reply.WrongLeader = false
+						 	reply.Value = value
+						 	reply.Err = OK
+					 	}
+				 	}
+				 	kv.mu.Unlock()
+			case <-time.After(1 * time.Second):
+				reply.Err = TimedOut
+			}
+	}
 }
 
 func (kv *KVServer) PutAppend(args *PutAppendArgs, reply *PutAppendReply) {
 	// Your code here.
+	fmt.Println("Put Append Procedure")
+	_, _, isLeader := kv.rf.Start(Op{args.Key, args.Value, args.Op, args.RequestNo, args.ClientId})
+	if isLeader == false {
+		fmt.Println("Wrong leader for PutAppend")
+		reply.WrongLeader = true
+	}else{
+		kv.mu.Lock()
+		fmt.Println("leader for PutAppend")
+		_, clientIDPresent := kv.ClientToResponseMapping[args.ClientId]
+
+		if clientIDPresent == false {
+			fmt.Println("Client ID not present for PutAppend")
+			ch := make(chan Response, 1)
+			kv.ClientToResponseMapping[args.ClientId] = ch
+		}
+		kv.mu.Unlock()
+
+		select {
+		case  response:= <- kv.ClientToResponseMapping[args.ClientId]:
+			if response.RequestNo == args.RequestNo && args.ClientId == response.ClientId && response.Key == args.Key{
+				reply.WrongLeader = false
+				reply.Err = OK
+				fmt.Println("No Duplicate")
+			} else {
+				fmt.Println("Duplicate")
+				reply.WrongLeader = false
+				reply.Err = Duplicate
+			}
+		case <- time.After(1 * time.Second): // Unreliable fails if we dont have this
+			reply.Err = TimedOut
+		}
+	}
+
 }
 
 //
@@ -53,6 +137,16 @@ func (kv *KVServer) PutAppend(args *PutAppendArgs, reply *PutAppendReply) {
 func (kv *KVServer) Kill() {
 	kv.rf.Kill()
 	// Your code here, if desired.
+}
+
+type Response struct {
+	value string
+	keyPresent bool
+	isDuplicate bool
+	RequestNo int
+	ClientId int64
+	Command string
+	Key string
 }
 
 //
@@ -82,8 +176,60 @@ func StartKVServer(servers []*labrpc.ClientEnd, me int, persister *raft.Persiste
 
 	kv.applyCh = make(chan raft.ApplyMsg)
 	kv.rf = raft.Make(servers, me, persister, kv.applyCh)
+	kv.DataStore = make(map[string]string)
+	kv.ClientToResponseMapping = make(map[int64] chan Response)
+	kv.duplicates = make(map[int64] int)
 
 	// You may need initialization code here.
+
+	go func() {
+		for{
+			msg := <- kv.applyCh
+			fmt.Println("Got applyCH")
+			op := msg.Command.(Op)
+			response := Response{value:"",RequestNo:op.RequestNo,ClientId:op.ClientId,Key:op.Key}
+			//For Get directly send the response
+			if op.TypeOfCommand!="Get" {
+				response.Command = op.TypeOfCommand
+				kv.mu.Lock()
+				requstNoStored, duplicatePresent := kv.duplicates[op.ClientId]
+				kv.mu.Unlock()
+				//check whether duplicate requestNo is there is or not if is there check the requestNo, it needs to be older then the requestNo
+				//we got from applyCh
+				if duplicatePresent == true{
+					//check whether requestNo stored is older or not if it is older then its not a duplicate respone
+					if requstNoStored >= op.RequestNo {
+						fmt.Println("Duplicate detected")
+						response.isDuplicate = true
+					} else if requstNoStored < op.RequestNo{
+						kv.mu.Lock()
+						response.isDuplicate = false
+						if op.TypeOfCommand == "Put" {
+							kv.DataStore[op.Key] = op.Value
+						}else if op.TypeOfCommand == "Append" {
+							kv.DataStore[op.Key] += op.Value
+						}
+						kv.duplicates[op.ClientId] = op.RequestNo
+						kv.mu.Unlock()
+					}
+				} else {
+					kv.mu.Lock()
+					if op.TypeOfCommand == "Put" {
+						kv.DataStore[op.Key] = op.Value
+					}else if op.TypeOfCommand == "Append" {
+							kv.DataStore[op.Key] += op.Value
+						}
+					kv.duplicates[op.ClientId] = op.RequestNo
+					kv.mu.Unlock()
+					}
+			}
+				select {
+					case kv.ClientToResponseMapping[op.ClientId] <- response:
+					default: // if you remove this it goes into deadlock
+				}
+
+		}
+	}()
 
 	return kv
 }
